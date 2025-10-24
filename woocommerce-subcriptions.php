1e9fc6a10deb3069719038eec1e862fd5e7ea231
UmerAliRevnix <umerali.revnix@gmail.com>
2025-10-24 12:08:28 -0700

    Added initial plugin file

---
+++ woocommerce-subcriptions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name:WooCommerce Subscriptions
+Plugin URI: https://github.com/UmerAliRevnix/WPAegis-Woocommerce-Subscriptions
+Description: Custom plugin integrating WooCommerce subscriptions with WPAegis.
+Version: 1.0
+Author: Umer Ali
+Author URI: https://github.com/UmerAliRevnix
+License: GPLv2 or later
+*/
